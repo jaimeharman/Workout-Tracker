69dfdd47d1241e7a4ca026fa80b8997b7cda038d
Jaime Harman <62770805+jaimeharman@users.noreply.github.com>
2020-10-18 17:27:14 -0400

    updated routes

---
+++ routes/api-routes.js
@@ -0,0 +1,61 @@
+const Workout = require('../models/Workout');
+
+module.exports = (app) => {
+    app.get('/api/workouts', (req, res) => {
+        Workout.find({})
+            .then((workouts) => {
+                workouts.forEach((workout) => {
+                    workout.totalDuration = 0;
+                    workout.exercises.forEach((exercise) => {
+                        workout.totalDuration += exercise.duration;
+                    });
+                });
+
+                res.json(workouts);
+            })
+            .catch((err) => {
+                res.json(err);
+            });
+    });
+
+    app.get('/api/workouts/range', (req, res) => {
+        Workout.find({})
+            .then((data) => {
+                res.json(data);
+            })
+            .catch(({ message }) => {
+                res.json(message);
+            });
+    });
+
+    app.post('/api/workouts', ({ body }, res) => {
+        Workout.create(body)
+            .then((data) => {
+                res.json(data);
+            })
+            .catch(({ message }) => {
+                res.json(message);
+            });
+    });
+
+    app.put('/api/workouts/:id', (req, res) => {
+        Workout.findById(req.params.id)
+            .then((data) => {
+                let workoutExercises = data.exercises;
+
+                workoutExercises.push(req.body);
+
+                Workout.findByIdAndUpdate(
+                    req.params.id,
+                    data,
+                    (err, result) => {
+                        if (err) res.json(err);
+                        else res.json(result);
+                    }
+                );
+            })
+            .catch(({ message }) => {
+                res.json(message);
+            });
+    });
+};
